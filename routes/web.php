3315184abe3bcc61e762fd3f41c7d892282ba0d7
lauroPereira <lauro.s.pereira@gmail.com>
2021-11-04 17:49:33 -0300

    feat laravel Cashier for subscription management

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Subscription\SubscriptionController;
 use Illuminate\Support\Facades\Route;
+use Laravel\Cashier\Subscription;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('subscriptions/classroom', [SubscriptionController::class, 'index']);
 
 Route::get('/', function () {
     return view('welcome');
